0ef247faf47c1168742e15cd2b6930cd7cb288c9
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2026-04-01 12:03:20 +0400

    add project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,98 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// PSEUDOCODE:
+
+// function LISTEN():
+//     while True:
+//         if MEM[@KBD] == 0:
+//             color = 0
+//         else:
+//             color = -1
+// 
+//         call FILL(color)
+// 
+// function FILL(color):
+//     start_address = @SCREEN
+//     count = 8192
+// 
+//     while count > 0:
+//         DECREMENT count
+// 
+//         current_address = start_address + count 
+// 
+//         MEM[current_address] = color
+// 
+//     return
+
+
+// Infinite loop listening for keyboard input.
+(LISTEN)
+    @KBD
+    D=M
+
+    @color
+    M=0
+
+    @LOOP_CONTINUE
+    D; JEQ
+
+    @color
+    M=-1
+
+(LOOP_CONTINUE)
+    @FILL
+    0; JMP
+
+(LISTEN_END)
+    @LISTEN_END
+    0; JMP
+
+
+// Color screen in @color.
+(FILL)
+    @SCREEN
+    D=A
+
+    @start_address
+    M=D
+
+    @8192
+    D=A
+    
+    @count
+    M=D
+
+(FILL_LOOP)
+    @count
+    D=M
+
+    @FILL_RETURN
+    D; JLE
+
+    @count
+    M=M-1
+
+    @start_address
+    D=M
+
+    @count
+    D=D+M
+
+    @current_address
+    M=D
+
+    @color
+    D=M
+
+    @current_address
+    A=M
+    M=D
+
+    @FILL_LOOP
+    0; JMP
+
+(FILL_RETURN)
+    @LISTEN
+    0; JMP
+
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,50 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+// PSEUDOCODE:
+
+// function MULT(a, b):
+//     count = b
+//     c = 0
+// 
+//     while count > 0:
+//         DECREMENT count
+// 
+//         c = c + a
+// 
+//     return c
+
+
+(MULT)
+    @R1
+    D=M
+
+    @count
+    M=D
+
+    @R2
+    M=0
+
+(LOOP)
+    @count
+    D=M
+
+    @END
+    D; JLE
+
+    @count
+    M=M-1
+
+    @R0
+    D=M
+
+    @R2
+    M=M+D
+
+    @LOOP
+    0; JMP
+
+(END)
+    @END
+    0; JMP
